2b277d6911d1ee4971367dbf8335c5379b2017bf
mamedov.r9657@gmail.com <2163osonivr7474rr>
2021-09-15 18:04:05 +0600

    Added Data Layer using Repository with Redis cache and BasketController with all routes to get/update/add basket to application cache.

---
+++ src/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task<ShoppingCart> GetBasketOrDefaultAsync(string userName)
+        {
+            var basket =  await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasketOrDefaultAsync(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await  _redisCache.RemoveAsync(userName);
+        }
+    }
+}
---
+++ src/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasketOrDefaultAsync(string userName);
+        Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
