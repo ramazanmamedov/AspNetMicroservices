6393c59a2402b7e8e1f91ea1f2192f26da7f1b59
mamedov.r9657@gmail.com <2163osonivr7474rr>
2021-09-15 16:02:50 +0600

    Implemented data layer using Repository pattern.

---
+++ src/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductAsync(string id);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string categoryName);
+
+        Task CreateProductAsync(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(string id);
+    }
+}
