5bfec5f17744c39974a88303b768308886c0cac0
mamedov.r9657@gmail.com <2163osonivr7474rr>
2021-09-18 15:00:06 +0600

    Added Repository Layer with CRUD operations. Added configuration to docker-compose.

---
+++ src/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+        Task<bool> DeleteDiscountAsync(string productName);
+    }
+}
