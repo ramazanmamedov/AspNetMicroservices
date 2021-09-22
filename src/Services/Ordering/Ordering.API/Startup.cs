0eacb93a2cb1a7583263f52592094d7db4e63665
ramazanmamedov <mamedov.r9657@gmail.com>
2021-09-22 18:41:44 +0600

    Created OrderController and added application layer services Dependency Injection.

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.Application;
 
 namespace Ordering.API
 {
@@ -26,6 +27,7 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
