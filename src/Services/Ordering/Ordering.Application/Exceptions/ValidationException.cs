b7e08741e3065b3bc9231a9e775a154f6f12b160
ramazanmamedov <mamedov.r9657@gmail.com>
2021-09-22 15:31:20 +0600

    Added Delete and Update command to CQRS pattern. Added custom exception to implement pre-processor and post-processor  behaviours.

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public Dictionary<string, string[]> Errors { get; }
+        public ValidationException() : base("One or more validation failures have occurred")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(group => group.Key, group => group.ToArray());
+        }
+    }
+}
